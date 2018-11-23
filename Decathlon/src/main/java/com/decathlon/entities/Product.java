db759397e61ae396043c75d9e4f0cff35e575f18
bhaskar <anvbhaskar@gmail.com>
2018-11-23 09:21:30 +0530

    EntityClassesCompleted

--- Decathlon/src/main/java/com/decathlon/entities/Product.java
+++ Decathlon/src/main/java/com/decathlon/entities/Product.java
@@ -1,20 +1,87 @@
 package com.decathlon.entities;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="product")
 public class Product {
 	
+	@Id
+	@Column(name="product_Id")
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer productId;
+	
+	@Column(name="product_Name")
 	private String productName;
 	
+	@Column(name="product_sport")
 	private String productSport;
 	
+	@Column(name="product_Level")
 	private String productLevel;
 	
+	@Column(name="product_description")
 	private String productDescription;
 	
+	@ManyToOne
+	@JoinColumn(name="associatedStores")
 	public Store associatedStores;
+
+	public Integer getProductId() {
+		return productId;
+	}
+
+	public void setProductId(Integer productId) {
+		this.productId = productId;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public String getProductSport() {
+		return productSport;
+	}
+
+	public void setProductSport(String productSport) {
+		this.productSport = productSport;
+	}
+
+	public String getProductLevel() {
+		return productLevel;
+	}
+
+	public void setProductLevel(String productLevel) {
+		this.productLevel = productLevel;
+	}
+
+	public String getProductDescription() {
+		return productDescription;
+	}
+
+	public void setProductDescription(String productDescription) {
+		this.productDescription = productDescription;
+	}
+
+	public Store getAssociatedStores() {
+		return associatedStores;
+	}
+
+	public void setAssociatedStores(Store associatedStores) {
+		this.associatedStores = associatedStores;
+	}
 	
 	
 }
--- Decathlon/src/main/java/com/decathlon/entities/Store.java
+++ Decathlon/src/main/java/com/decathlon/entities/Store.java
@@ -1,5 +1,62 @@
 package com.decathlon.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="store")
 public class Store {
 
+	@Id
+	@Column(name="store_Id")
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer storeId;
+	
+	@Column(name="store_Name")
+	private String storeName;
+	
+	@Column(name="store_Description")
+	private String storeDescription;
+	
+	@OneToMany(mappedBy="associatedStores")
+	public Product products;
+
+	public Integer getStoreId() {
+		return storeId;
+	}
+
+	public void setStoreId(Integer storeId) {
+		this.storeId = storeId;
+	}
+
+	public String getStoreName() {
+		return storeName;
+	}
+
+	public void setStoreName(String storeName) {
+		this.storeName = storeName;
+	}
+
+	public String getStoreDescription() {
+		return storeDescription;
+	}
+
+	public void setStoreDescription(String storeDescription) {
+		this.storeDescription = storeDescription;
+	}
+
+	public Product getProducts() {
+		return products;
+	}
+
+	public void setProducts(Product products) {
+		this.products = products;
+	}
+	
+	
 }
