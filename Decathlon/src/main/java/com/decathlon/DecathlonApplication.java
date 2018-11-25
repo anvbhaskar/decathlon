4c4992b3b8c102a980ceb672b2a76470a083e8f7
nani <nani@Bhaskar.local>
2018-11-25 12:08:20 +0530

    EurekaClientCahgnes

--- Decathlon/src/main/java/com/decathlon/DecathlonApplication.java
+++ Decathlon/src/main/java/com/decathlon/DecathlonApplication.java
@@ -3,9 +3,11 @@ package com.decathlon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableAutoConfiguration
+@EnableEurekaClient
 public class DecathlonApplication {
 
 	public static void main(String[] args) {
